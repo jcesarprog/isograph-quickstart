42a3dcab6f6ec0b074cf2400193133d062bfe421
Robert Balicki <robert.balicki@gmail.com>
2024-02-07 12:21:22 -0500

    modify _app.tsx to teach Isograph how to hit the Star Wars API

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { useMemo } from 'react';
+import type { AppProps } from 'next/app';
+import {
+  createIsographEnvironment,
+  createIsographStore,
+  IsographEnvironmentProvider,
+} from '@isograph/react';
+
+function makeNetworkRequest<T>(queryText: string, variables: any): Promise<T> {
+  let promise = fetch(
+    'https://swapi-graphql.netlify.app/.netlify/functions/index',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: queryText, variables }),
+    },
+  ).then((response) => response.json());
+  return promise;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const environment = useMemo(
+    () => createIsographEnvironment(createIsographStore(), makeNetworkRequest),
+    [],
+  );
+  return (
+    <IsographEnvironmentProvider environment={environment}>
+      <Component {...pageProps} />
+    </IsographEnvironmentProvider>
+  );
 }
