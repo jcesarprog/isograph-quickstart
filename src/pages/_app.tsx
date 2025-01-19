8a9f290b5fc3600cf0256712a24fa5d0bbd2c9bb
Robert Balicki <robert.balicki@gmail.com>
2025-01-19 10:49:45 -0800

    upgrade network function to have proper error handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,44 @@
-import { useMemo } from 'react';
-import type { AppProps } from 'next/app';
+import { useMemo } from "react";
+import type { AppProps } from "next/app";
 import {
   createIsographEnvironment,
   createIsographStore,
   IsographEnvironmentProvider,
-} from '@isograph/react';
+} from "@isograph/react";
 
 function makeNetworkRequest<T>(queryText: string, variables: any): Promise<T> {
   let promise = fetch(
-    'https://swapi-graphql.netlify.app/.netlify/functions/index',
+    "https://swapi-graphql.netlify.app/.netlify/functions/index",
     {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ query: queryText, variables }),
-    },
-  ).then((response) => response.json());
+    }
+  ).then(async (response) => {
+    const json = await response.json();
+
+    if (response.ok) {
+      if (json.errors != null) {
+        throw new Error("GraphQLError", {
+          cause: json.errors,
+        });
+      }
+      return json;
+    } else {
+      throw new Error("NetworkError", {
+        cause: json,
+      });
+    }
+  });
   return promise;
 }
 
 export default function App({ Component, pageProps }: AppProps) {
   const environment = useMemo(
     () => createIsographEnvironment(createIsographStore(), makeNetworkRequest),
-    [],
+    []
   );
   return (
     <IsographEnvironmentProvider environment={environment}>
