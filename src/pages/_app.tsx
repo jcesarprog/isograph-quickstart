cef2ef2a0b3318d19b261c3b4de1c892010c1e01
Julio Teixeira <jcesarprog@gmail.com>
2025-08-24 17:18:07 -0300

    refactor: _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,65 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import {
+  createIsographEnvironment,
+  createIsographStore,
+  IsographEnvironmentProvider,
+} from '@isograph/react';
+import type { AppProps } from 'next/app';
+import { Suspense, useMemo } from 'react';
+
+function makeNetworkRequest<T>(
+  queryText: string,
+  variables: unknown,
+): Promise<T> {
+  const promise = fetch('https://graphql.org/graphql/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query: queryText, variables }),
+  }).then(async (response) => {
+    const json = await response.json();
+
+    if (response.ok) {
+      /**
+       * Enforce that the network response follows the specification:: {@link https://spec.graphql.org/draft/#sec-Errors}.
+       */
+      if (Object.hasOwn(json, 'errors')) {
+        if (!Array.isArray(json.errors) || json.errors.length === 0) {
+          throw new Error('GraphQLSpecificationViolationError', {
+            cause: json,
+          });
+        }
+        throw new Error('GraphQLError', {
+          cause: json.errors,
+        });
+      }
+      return json;
+    }
+    throw new Error('NetworkError', {
+      cause: json,
+    });
+  });
+  return promise;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const environment = useMemo(
+    () =>
+      createIsographEnvironment(
+        createIsographStore(),
+        makeNetworkRequest,
+        // Optional missing field handler
+        null,
+        // Optional logger
+        console.log,
+      ),
+    [],
+  );
+  return (
+    <IsographEnvironmentProvider environment={environment}>
+      <Suspense fallback="loading">
+        <Component {...pageProps} />
+      </Suspense>
+    </IsographEnvironmentProvider>
+  );
 }
